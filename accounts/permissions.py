ff7c056f906cd745be06b7d77eeac7161a3ac101
Neperienx <112650202+Neperienx@users.noreply.github.com>
2026-01-16 14:26:55 +0100

    Improve error messaging and login guidance

--- accounts/permissions.py
+++ accounts/permissions.py
@@ -1,5 +1,9 @@
 from functools import wraps
-from django.core.exceptions import PermissionDenied
+
+from django.contrib import messages
+from django.contrib.auth import REDIRECT_FIELD_NAME
+from django.contrib.auth.views import redirect_to_login
+from django.urls import reverse
 
 
 def require_manager_or_owner(view_func):
@@ -7,7 +11,15 @@ def require_manager_or_owner(view_func):
     def _wrapped(request, *args, **kwargs):
         user = request.user
         if not user.is_authenticated or not user.is_manager_or_owner():
-            raise PermissionDenied("You do not have access to this page.")
+            messages.error(
+                request,
+                "This page is only available to shop managers or owners. "
+                "Log in with a manager or owner account to continue.",
+            )
+            login_url = reverse("account_login")
+            return redirect_to_login(
+                request.get_full_path(), login_url, REDIRECT_FIELD_NAME
+            )
         return view_func(request, *args, **kwargs)
 
     return _wrapped
