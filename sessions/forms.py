a533b28664873ec9c6542f4329ecb0c63a360d42
Nicolas Nikeola <nicolas.leteinturier2.0@gmail.com>
2026-01-26 11:46:27 +0100

    start new

--- sessions/forms.py
+++
@@ -1,8 +0,0 @@
-from django import forms
-from .models import BrideSession
-
-
-class BrideSessionForm(forms.ModelForm):
-    class Meta:
-        model = BrideSession
-        fields = ["bride_name"]
